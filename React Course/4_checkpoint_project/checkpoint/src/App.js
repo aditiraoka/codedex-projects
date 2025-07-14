d2ae3aca7140f614c4b6f52fed61dcb14746c7dc
Aditirao Kalanji <aditirao19@gmail.com>
2025-07-13 20:55:45 -0500

    Added some event handlers for checkpoint project

--- React Course/4_checkpoint_project/checkpoint/src/App.js
+++ React Course/4_checkpoint_project/checkpoint/src/App.js
@@ -3,6 +3,9 @@ import React, {useState} from "react";
 
 function App() {
   const [progress, setProgress] = useState(0);
+  const [increaseHovered, setIncreaseHovered] = useState(false)
+  const [resetHovered, setResetHovered] = useState(false)
+
   function handleProgress() {
     if(progress<100){
       setProgress(progress+10);
@@ -14,11 +17,40 @@ function App() {
     setProgress(0);
   }
 
+  function handleIncreaseMouseEnter() {
+    setIncreaseHovered(true);
+  }
+
+  function handleIncreaseMouseLeave() {
+    setIncreaseHovered(false);
+  }
+
+  function handleResetMouseEnter() {
+    setResetHovered(true);
+  }
+
+  function handleResetMouseLeave() {
+    setResetHovered(false);
+  }
+
   return (
     <div>
       <ProgressBar progress={progress}/>
-      <button className="button" onClick={handleProgress} style={{backgroundColor:"green"}}>Increase Progress</button> <br/>
-      <button className="button" onClick={resetProgress} style={{backgroundColor:"red"}}>Reset Progress</button>
+      <button className={`increase-button ${increaseHovered? 'hovered':''}`} 
+      onClick={handleProgress}
+      onMouseEnter={handleIncreaseMouseEnter}
+      onMouseLeave={handleIncreaseMouseLeave}>
+        Increase Progress
+      </button>
+      
+      <button className={`reset-button ${resetHovered? "hovered":""}`}
+      tabIndex="0"
+      onClick={resetProgress} 
+      onMouseEnter={handleResetMouseEnter}
+      onMouseLeave={handleResetMouseLeave}
+      style={{marginLeft:"10px"}}>
+        Reset Progress
+      </button>
     </div>
   );
 }
--- React Course/4_checkpoint_project/checkpoint/src/ProgressBar.js
+++ React Course/4_checkpoint_project/checkpoint/src/ProgressBar.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 export default function ProgressBar(props) {
+    const [showTooltip, setShowTooltip] = useState(false);
+    function handleMouseEnter(){
+        setShowTooltip(true);
+    }
+    function handleMouseLeave(){
+        setShowTooltip(false);
+    }
+
     return (
         <div>
             <h1>Progress Bar</h1>
-            <div className="progress-bar" style={{ width: "50%", alignItems: "center" }}>
+            <div className="progress-bar" style={{ width: "50%", alignItems: "center" }}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <div
                 style={{
                     width: `${props.progress}%`,
                     backgroundColor: "#007bff",
                     height: "24px",
                     borderRadius: "4px",
-                }}
-                ></div>
+                }}>
+                </div>
+                
             </div>
+            {showTooltip && <div className="tooltip">You still have {100 - props.progress}% to go!</div>}
             <p>{props.progress}% Complete</p>
         </div>
     );
